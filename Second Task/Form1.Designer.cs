3ceb4fa0f8f82142d86cf94715327b54e5b9df3f
Dodonova19 <dodonova.vikyss@mail.ru>
2021-09-30 18:01:10 +0800

    Создан интерфейс. Добавлены действия при нажатии на кнопку.

--- Second Task/Form1.Designer.cs
+++ Second Task/Form1.Designer.cs
@@ -1,7 +1,6 @@
-﻿
-namespace Second_Task
+﻿namespace Second_Task
 {
-    partial class Form1
+    partial class TaskForm
     {
         /// <summary>
         ///  Required designer variable.
@@ -29,13 +28,77 @@ namespace Second_Task
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.taskLable = new System.Windows.Forms.Label();
+            this.solveButton = new System.Windows.Forms.Button();
+            this.nInput = new System.Windows.Forms.TextBox();
+            this.solveLable = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // taskLable
+            // 
+            this.taskLable.AutoSize = true;
+            this.taskLable.Location = new System.Drawing.Point(12, 9);
+            this.taskLable.Name = "taskLable";
+            this.taskLable.Size = new System.Drawing.Size(368, 15);
+            this.taskLable.TabIndex = 0;
+            this.taskLable.Text = "Среди чисел 1, 4, 9, 16, 25, ... найти первое число, большее чем N";
+            // 
+            // solveButton
+            // 
+            this.solveButton.Enabled = false;
+            this.solveButton.Location = new System.Drawing.Point(305, 36);
+            this.solveButton.Name = "solveButton";
+            this.solveButton.Size = new System.Drawing.Size(75, 23);
+            this.solveButton.TabIndex = 2;
+            this.solveButton.Text = "Найти";
+            this.solveButton.UseVisualStyleBackColor = true;
+            this.solveButton.Click += new System.EventHandler(this.solveButton_Click);
+            // 
+            // nInput
+            // 
+            this.nInput.Location = new System.Drawing.Point(12, 36);
+            this.nInput.MaxLength = 10;
+            this.nInput.Name = "nInput";
+            this.nInput.Size = new System.Drawing.Size(281, 23);
+            this.nInput.TabIndex = 1;
+            this.nInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.nInput.TextChanged += new System.EventHandler(this.nInput_TextChanged);
+            // 
+            // solveLable
+            // 
+            this.solveLable.AutoSize = true;
+            this.solveLable.Location = new System.Drawing.Point(15, 77);
+            this.solveLable.Name = "solveLable";
+            this.solveLable.Size = new System.Drawing.Size(0, 15);
+            this.solveLable.TabIndex = 3;
+            // 
+            // TaskForm
+            // 
+            this.AcceptButton = this.solveButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(386, 111);
+            this.Controls.Add(this.solveLable);
+            this.Controls.Add(this.nInput);
+            this.Controls.Add(this.solveButton);
+            this.Controls.Add(this.taskLable);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "TaskForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Задача на for";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label taskLable;
+        private System.Windows.Forms.Button solveButton;
+        private System.Windows.Forms.TextBox nInput;
+        private System.Windows.Forms.Label solveLable;
     }
 }
 
--- Second Task/Form1.cs
+++ Second Task/Form1.cs
@@ -10,12 +10,42 @@ using System.Windows.Forms;
 
 namespace Second_Task
 {
-    public partial class Form1 : Form
+    public partial class TaskForm : Form
     {
-        public Form1()
+        public TaskForm()
         {
             InitializeComponent();
+            nInput.Text = Properties.Settings.Default.n.ToString();
+            if (nInput.Text.Length != 0)
+            {
+                solveLable.Text = "Искомое число: " + Logic.Solve(int.Parse(nInput.Text));
+            }
         }
 
+        private void solveButton_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(nInput.Text, out int n))
+            {
+                solveLable.Text = "Искомое число: " + Logic.Solve(n);
+                Properties.Settings.Default.n = nInput.Text;
+                Properties.Settings.Default.Save();
+            }
+            else
+            {
+                MessageBox.Show("В строке должны быть только цифры", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void nInput_TextChanged(object sender, EventArgs e)
+        {
+            if (nInput.Text.Length > 0)
+            {
+                solveButton.Enabled = true;
+            }
+            else
+            {
+                solveButton.Enabled = false;
+            }
+        }
     }
 }
